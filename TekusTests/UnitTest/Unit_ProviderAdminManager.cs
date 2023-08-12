e224bc074ed59330b5219bffedd1ed960a30acdf
Juan Ignacio DAmato <juanidamato@gmail.com>
2023-08-12 09:56:07 -0500

    test unitario

---
+++ TekusTests/UnitTest/Unit_ProviderAdminManager.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TekusCore.Application.BLL;
+using TekusCore.Application.Features.Providers.Querys;
+using TekusCore.Application.Interfaces.Repositories;
+using TekusCore.Domain.Commons;
+using TekusCore.Domain.Entities;
+
+namespace TekusTests.UnitTest
+{
+   
+    public class Unit_ProviderAdminManager
+    {
+        [Fact]
+        public async Task ProviderAdminManager_ProvidingInvalidPaginationValue_ReturnError()
+        {
+            //arrange
+            Mock<ILogger<ProviderAdminManager>> logger = new Mock<ILogger<ProviderAdminManager>>();
+            Mock<IProviderRepository> repo = new Mock<IProviderRepository>();
+            OperationStatusModel op;
+            List<ProviderEntity>? data;
+            GetProvidersAdminQuery request = new GetProvidersAdminQuery();
+            request.Page = 0;
+            request.RecordsPerPage = 1000;
+            request.SortDirection = "ASCX";
+            request.SortBy = "NAMEX";
+
+            //act
+            var sut=new ProviderAdminManager(logger.Object,repo.Object);
+            (op, data) = await sut.GetProvidersAdmin(request);
+
+            //assert
+            Assert.NotNull(op);
+            Assert.Null(data);
+            Assert.Equal(OperationResultCodes.BAD_REQUEST, op.code);
+        }
+    }
+}
